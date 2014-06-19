a931547a931702eecfa515cccc7f8cadae6709e9
Alex McRitchie <amcritchie@gmail.com>
2014-06-19 14:35:00 -0600

    Commiting split warmup.

--- split_join_warmup.rb
+++ split_join_warmup.rb
@@ -10,31 +10,49 @@ world_cup_news
 
 # enter your solutions inside the methods
 def goal(message)
-
+  message*2
 end
 
 def generate_an_array_of_teams(teams)
-
+  teams_array = teams.split(", ")
 end
 
 def number_of_teams(teams)
-
+  teams_array = teams.split(", ").length
 end
 
 def return_australia(teams)
-
+  teams_array = teams.split(", ")[6]
 end
 
 def starts_with_C(teams)
+  teams_array = teams.split(", ")
+  array =[]
+  teams_array.each do |team|
+    if team[0]  == 'C'
+      array << team
+    end
+  end
+  array
 
+  #teams.select {|team team[0] == 'C'}
 end
 
 def block_string_to_single_line(block_string)
+  block_string.split("\n").join(",").split(",").join(" ").split(" ").join(" ")
 
 end
 
 def capitalize_every_third_word(block_string)
-
+  array = block_string.split("\n").join(",").split(",").join(" ").split(" ").join(" ").downcase. split(" ")
+  mod = 0
+  array.each do |word|
+    if (mod % 3) == 0
+      array[mod] = word.capitalize
+    end
+    mod += 1
+  end
+  array.join(" ")
 end
 
 check("goal method", goal(goal_message) == "GOOOOOOOAL!!!!GOOOOOOOAL!!!!")
